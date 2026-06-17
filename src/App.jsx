1786e73df362b36fc0c259d4a16c85afff095655
Jessica Ferreira <fjes.ks@gmail.com>
2026-06-17 17:23:07 -0300

    multiple get requests with axios

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ function App() {
 
   useEffect (() => {
     setLoading(true);
-
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response =>  {
-        console.log(response);
-        setData(response.data);
+    axios.all([
+      axios.get('https://jsonplaceholder.typicode.com/posts'),
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      ])
+    .then(axios.spread ((posts, users) =>  {
+        console.log(posts);
+        console.log(users);
+        setData(posts.data);
         setLoading(false);
-      })
+      }))
       .catch((error) => {
         console.log ('Error fetching data:', error);
         setError ('Failed to fetch the data');
