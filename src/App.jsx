1d42c80c1e8fb099a8b8ef21d31a51676c630502
Jessica Ferreira <fjes.ks@gmail.com>
2026-06-17 16:42:40 -0300

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import './App.css';
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [error, setError] = useState(null);
 
   useEffect (() => {
+    setLoading(true);
+
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json =>  {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log ('Error fetching data:', error);
+        setError ('Failed to fetch the data');
+        setLoading(false);
+      });
   }, []);
 
   if (loading) {
     return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
   }
 
   return (
